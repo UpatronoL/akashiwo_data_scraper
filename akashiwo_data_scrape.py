6d05434089931d6a50138ef48083b05238c9a012
UpatronoL <118782554+UpatronoL@users.noreply.github.com>
2023-10-24 13:47:07 +0900

    geting some of the things i deleted by combining the branches

--- akashiwo_data_scrape.py
+++ akashiwo_data_scrape.py
@@ -46,19 +46,3 @@ def get_id_date(id_array, date_array, file_name):
     data = pd.read_csv(file_name)
     id_array.append(data['pointId'])
     date_array.append(data['gatherYMD'])
-
-###
-# data_array - any array that you would like to populate with data
-# html_source - html that you have gathered
-###
-def data_parser(data_array, html_source, ):
-    # Finds all existing tables in the html
-    tables = html_source.find_all('table')
-
-    # Finds all the wanted data in a table
-    for table in tables:
-        rows = table.find_all('tr')
-        for row in rows:
-            element = row.find_all(['th', 'td'])
-            data = [i.text for i in element]
-            data_array.append(data)
